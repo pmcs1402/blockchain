b9ec32a3970065f6faa87d3e85a3b3467423bbf5
Chol Hyun Park <chpark@westmont.edu>
2018-04-09 07:34:18 -0700

    Update BlockHeader.java

--- BlockHeader.java
+++ BlockHeader.java
@@ -1,23 +1,10 @@
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class BlockchainDriver {
+public class BlockHeader {
+	  private int version;              
+	  private byte[] previousBlockHash;
+	  private int merkleRootHash;
+	  private int timestamp;           
+	  private int difficultyTarget;    
+	  private int nonce;                
 
-
-	  List<block> blockchain = new ArrayList<block>();
-
-	  public static void main(String[] args) throws NoSuchAlgorithmException {
-
-	    // Genesis block
-	    String[] transactions = {"Hosang sent 1k Bitcoins to Zuckerberg."};
-	    block genesisBlock = new block(new BlockHeader(null, transactions), transactions);
-	    System.out.println("Block Hash : " + genesisBlock.getBlockHash());
-
-	    // Transaction Forgery
-	    transactions[0] = "Hosang sent 10k Bitcoins to Zuckerberg.";
-	    genesisBlock = new block(new BlockHeader(null, transactions), transactions);
-	    System.out.println("Block Hash : " + genesisBlock.getBlockHash());
-
-	  }
 }
